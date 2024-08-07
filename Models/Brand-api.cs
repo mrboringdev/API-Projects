bb6aeb2355ea0e4aed0827bd5b5a8e7dec2478ad
mrboringdev <hussainshaik206@gmail.com>
2024-08-08 00:16:31 +0530

    Add project files.

---
+++ Models/Brand-api.cs
@@ -0,0 +1,12 @@
+﻿namespace SummaryProfileAPI.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+
+        public string? category { get; set; }
+
+        public  int IsActive { get; set; }
+    }
+}
